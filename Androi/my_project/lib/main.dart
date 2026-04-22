5f624fcaad36b7243213e8db05fb3d246a4a797d
AnNguyen264IT <npan264work@gmail.com>
2026-04-22 10:17:46 +0700

    update

--- Androi/my_project/lib/main.dart
+++ Androi/my_project/lib/main.dart
@@ -55,7 +55,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+   String message = ""; // biến lưu nội dung hiển thị
 
+  void _showHello() {
+    setState(() {
+      if (message.isEmpty) {
+      message = "Xin chào";
+    } else {
+      message = "";
+    }
+    });
+  }
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -89,33 +99,21 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('Nhấn nút để hiển thị:'),
+             const SizedBox(height: 20),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              message,
+              style: const TextStyle(fontSize: 24, color: Colors.blue),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _showHello,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.message),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
